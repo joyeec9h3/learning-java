d5ce19060df612b938f3a3ce4a3428dfbee4f789
Joyee Cheung <joyeec9h3@gmail.com>
2014-07-14 06:48:25 +0800

    fix the plural bug

--- Beers/src/Beers.java
+++ Beers/src/Beers.java
@@ -6,13 +6,14 @@ public class Beers {
 	
 	public static void sing(int bottle) {
 		System.out.printf("\n%d bottles of beer on the wall, %d bottles of beer,", bottle, bottle);
-		System.out.printf(" ya’ take one down, ya’ pass it around, 98 bottles of beer on the wall.\n", bottle - 1);
+		System.out.printf(" ya’ take one down, ya’ pass it around, %d ", bottle - 1);
+		System.out.printf("bottle%s of beer on the wall.\n", bottle - 1 > 1 ? "s" : "");
 	}
 	
 	public static void singTheEnd() {
-		System.out.print("\nNo bottles of beer on the wall, no bottles of beer, ");
-		System.out.print("ya’ can’t take one down, ya’ can’t pass it around, ");
-		System.out.print("’cause there are no more bottles of beer on the wall! \n");
+		System.out.printf("\nNo bottles of beer on the wall, no bottles of beer, ");
+		System.out.printf("ya’ can’t take one down, ya’ can’t pass it around, ");
+		System.out.printf("’cause there are no more bottles of beer on the wall! \n");
 	}
 	
 	public static void startSinging(int bottle) {
