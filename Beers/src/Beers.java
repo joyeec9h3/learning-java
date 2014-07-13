fefa865ad30ec0e903fbff8019a562f353cf4549
Joyee Cheung <joyeec9h3@gmail.com>
2014-07-14 06:42:58 +0800

    change encoding to UTF-8

--- Beers/src/Beers.java
+++ Beers/src/Beers.java
@@ -6,13 +6,13 @@ public class Beers {
 	
 	public static void sing(int bottle) {
 		System.out.printf("\n%d bottles of beer on the wall, %d bottles of beer,", bottle, bottle);
-		System.out.printf("ya¡¯ take one down, ya¡¯ pass it around, %d bottles of beer on the wall.\n", bottle - 1);
+		System.out.printf(" yaâ€™ take one down, yaâ€™ pass it around, 98 bottles of beer on the wall.\n", bottle - 1);
 	}
 	
 	public static void singTheEnd() {
 		System.out.print("\nNo bottles of beer on the wall, no bottles of beer, ");
-		System.out.print("ya¡¯ can¡¯t take one down, ya¡¯ can¡¯t pass it around, ");
-		System.out.print("¡¯cause there are no more bottles of beer on the wall!\n");
+		System.out.print("yaâ€™ canâ€™t take one down, yaâ€™ canâ€™t pass it around, ");
+		System.out.print("â€™cause there are no more bottles of beer on the wall! \n");
 	}
 	
 	public static void startSinging(int bottle) {
